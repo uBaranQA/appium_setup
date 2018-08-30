72d883427b2e4c374126a09e4ac5eb8dc44fbe4f
Piotr Baran <piotr.baran@jcommerce.pl>
2018-08-30 21:44:49 +0200

    Test Name Rule added

--- src/test/java/pl/ubaranqa/appium_setup/TestBase.java
+++ src/test/java/pl/ubaranqa/appium_setup/TestBase.java
@@ -5,7 +5,9 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -14,11 +16,15 @@ import java.net.URL;
 
 public class TestBase {
 
+    private AppiumDriver driver;
+
+    /* Sets the test name to the name of the test method. */
+    @Rule
+    public TestName testName = new TestName();
+
     private static final String APPIUM_REMOTE_URL = "https://eu1.appium.testobject.com/wd/hub";
     private static final String APPIUM_URL = "http://127.0.0.1:4723/wd/hub";
     private static final String API_KEY = "3B677A8960FD427E824BCE378F643677";
-
-    private AppiumDriver driver;
 
     private final static String EXPECTED_RESULT_FOUR = "4";
 
